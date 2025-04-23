c2fcabcafd77374ce22b504cfe4d757eaac5a745
ximavus <ximavus27@gmail.com>
2025-04-23 16:38:42 +0300

    added a purpose for the useless main.py file
    Added ACTUAL CODE!!!

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+from time import sleep
+from mcpi import minecraft
+mc = minecraft.Minecraft.create()
+mc.postToChat("Now the chat is python!")
+while True:
+    chat = str(mc.events.pollChatPosts())[32:-2]
+    try:
+        sleep(0.01)
+        exec(chat.replace("print", "mc.postToChat").replace("; ", "\n"))
+    except Exception as e:
+        mc.postToChat(e)
