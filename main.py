d389c95a07e9535597c47af21a6b63e0b3c5fb64
ximavus <ximavus27@gmail.com>
2025-07-07 12:26:24 +0300

    made main.py be the text editor
    the title says it all

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from time import sleep
-from mcpi import minecraft
-mc = minecraft.Minecraft.create()
-mc.postToChat("Now the chat is python!")
-while True:
-    chat = str(mc.events.pollChatPosts())[32:-2]
-    try:
-        sleep(0.01)
-        exec(chat.replace("print", "mc.postToChat").replace("; ", "\n"))
-    except Exception as e:
-        mc.postToChat(e)
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+root.title('Minimal text editor!')
+root.geometry("1920x1080")
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+global textarea
+textarea = Text(frm, background='black', foreground='white', width=200, height=56)
+textarea.grid()
+def run():
+    exec(textarea.get('1.0', 'end-1c'))
+global filename_entry
+filename_entry = ttk.Entry(frm)
+filename_entry.grid(column=2, row=1)
+global filename
+filename = ''
+def save():
+    filename = filename_entry.get()
+    print(filename)
+    print(textarea.get('1.0', 'end-1c'))
+    makefile(filename, textarea.get('1.0', 'end-1c'))
+def makefile(name: str, contents: str):
+    file = open(name, 'w')
+    file.write(contents)
+    file.close()
+ttk.Button(frm, text="Save", command=save).grid(column=1, row=3)
+ttk.Button(frm, text="Run", command=run).grid(column=1, row=1)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
+root.mainloop()
